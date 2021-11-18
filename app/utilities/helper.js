09bd755abff9bd0227a18f166d80e2fb90f55cdd
mkaubr007 <mkaubr007@gmail.com>
2021-11-19 00:14:32 +0530

    Refactor helper and service

--- app/utilities/helper.js
+++ app/utilities/helper.js
@@ -1,3 +1,4 @@
+const jwt=require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 class Helper {
     hashing = (password, callback) => {
@@ -8,6 +9,15 @@ class Helper {
           return callback(null, hash);
         }
       });
+    }
+    token = (data) => {
+      const dataForToken = {
+        id: data._id,
+        firstName: data._firstName,
+        lastName: data._lastName,
+        email: data._email
+      };
+      return jwt.sign({ dataForToken }, process.env.JWT_SECRET, { expiresIn: '24H' });
     }
 }
 module.exports = new Helper();
