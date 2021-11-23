c1abce9d56cf9ad5f77d166f6711846ded55539e
mkaubr007 <mkaubr007@gmail.com>
2021-11-23 22:44:03 +0530

    Refactor: Added logger

--- app/models/note.model.js
+++ app/models/note.model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const utilities = require('../utilities/helper.js');
-
+const { logger } = require('../../logger/logger');
 const userSchema = mongoose.Schema({
   firstName: {
     type: String,
@@ -50,6 +50,7 @@ class userModel {
       });
       }
       catch (error) {
+        logger.error('Find error in model');
           return callback('Internal Error', null)
       }
   }  
@@ -57,10 +58,15 @@ class userModel {
   loginUser= (loginData, callBack) => {
     //To find a user email in the database
     User.findOne({ email: loginData.email }, (error, data) => {
-        if (data) {
-            return callBack(null, data);           
-        } else{
-            return callBack("Invalid email", null);
+        if (error) {
+          logger.error('Find error while loggin user');
+            return callBack(error, null);           
+        } else if(!data){
+          logger.error('Invalid User');
+            return callBack("Invalid Credential", null);
+        }else{
+          logger.info('Email id found');
+            return callBack(null,data);
         }
     });
 }
