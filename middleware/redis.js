4a29c9bc49e5c6cbd4f111cb2b6f78d4ac82069b
mkaubr007 <mkaubr007@gmail.com>
2021-12-17 22:27:05 +0530

    feat:Added redis for getNoteByID

---
+++ middleware/redis.js
@@ -0,0 +1,57 @@
+/* eslint-disable camelcase */
+const redis = require('redis');
+const client = redis.createClient();
+const { logger } = require('../../logger/logger');
+
+class Redis {
+  /**
+     * @description function written to provide data to user in minimal time using caching
+     * @param {*} a req valid request is expected
+     * @param {*} res depends on the request of user
+     * @param {*} if there is no data function calls for next function
+     */
+   redis_NOteById = (req, res, next) => {
+     const noteId = req.params.id;
+     client.get(noteId, (error, redis_data) => {
+       if (error) {
+         logger.error(error);
+         throw error;
+       } else if (redis_data) {
+         logger.info('getLabels successfully retrieved');
+         res.status(200).send({
+           redis_NoteById: JSON.parse(redis_data),
+           message: 'getlabels successfully retrieved',
+           success: true
+         });
+       } else {
+         next();
+       }
+     });
+   }
+
+
+ /**
+    * @description setting data to key into redis
+    * @param userId
+    * @param data
+    */
+
+  setData = (key, time, redis_data) => {
+    client.setex(key, time, redis_data);
+  };
+
+   /**
+    * @description clearing cache
+    */
+
+    clearCache = (key) => {
+        client.del(key, (err, res) => {
+          if (err) {
+            logger.error('cache not cleared');
+          } else {
+            console.log('Cache cleared');
+            logger.info('Cache cleared');
+          }
+        });
+      }
+}
